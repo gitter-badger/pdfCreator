763238d3cea88c1433e2d3c99060dfd14a9b0bf1
Muhammad Aref <muh.aref@live.com>
2016-07-20 00:26:13 +0200

    #1: Handle splitting the long list of items into multiple pages

--- src/layouts/titleAndParagList.js
+++ src/layouts/titleAndParagList.js
@@ -10,30 +10,74 @@
 
     PDF.addLayout('titleAndParagList', function (data) {
         const {doc, width, height, padding} = this,
-            {titleColor = [0, 0, 0], list = [], lineNumbers} = data;
+            {titleColor = [0, 0, 0], list = [], lineNumbers, pagesLimit = Infinity} = data;
 
         // Track and update posY, after adding each title/parag
         let posY = padding + 55;
 
-        list.forEach((item, index) => {
-            const titleHeight = this.insertText({
+        // Track the added pages, so that we won't add page more than the pagesLimit
+        let addedPages = 1;
+
+        // Using some so we can break the iteration when the added pages exceed the pagesLimit
+        list.some((item, index) => {
+            let titleHeight = this.getTextHeight({
+                text: (lineNumbers ? index + 1 + '. ' : '') + item.title,
+                fontSize: 15,
+                type: 'bold',
+                posX: padding
+            });
+
+            let paragHeight = this.getTextHeight({
+                text: item.parag,
+                fontSize: 10,
+                type: 'normal',
+                posX: padding
+            });
+
+            // Check if there is no room for extra text, then create new page
+            if (titleHeight + paragHeight > this.height - this.padding * 2 - posY) {
+                if (addedPages++ === pagesLimit) return true;
+
+                // Add new page
+                this.addPage({
+                    width: this.width, 
+                    height: this.height
+                });
+                
+                // Insert page header
+                this.insertHeader({
+                    text:'Socialbakers Export'
+                });
+
+                // Insert page footer
+                this.insertFooter({
+                    text: 'Page 4/10',
+                    align: 'center'
+                });
+
+                posY = padding + 55;
+            }
+       
+            titleHeight = this.insertText({
                 text: (lineNumbers ? index + 1 + '. ' : '') + item.title,
                 fontSize: 15,
                 posX: padding,
                 posY, 
                 color: titleColor,
-                type: 'bold'
+                type: 'bold',
+                maxAllowedHeight: this.height - this.padding * 2 - posY
             });
 
             posY += titleHeight;
 
-            const paragHeight = this.insertText({
+            paragHeight = this.insertText({
                 text: item.parag,
                 fontSize: 10,
                 posX: padding,
                 posY,
-                type: 'normal'
-            });
+                type: 'normal',
+                maxAllowedHeight: this.height - this.padding * 2 - posY
+            });       
 
             posY += paragHeight + 20;
         });
