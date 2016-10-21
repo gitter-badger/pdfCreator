382510d956a334530237b902ae254bf432be95c9
Muhammad Aref Ali Hamada <muh.aref@live.com>
2016-10-21 20:11:26 +0200

    chore(code-style): defining code-style and integrate eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-const gulp = require('gulp'),
-	babel = require('gulp-babel'),
-	babelify = require('babelify'),
-	browserify = require('gulp-browserify');
+const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const eslint = require('gulp-eslint');
+const babel = require('gulp-babel');
+const babelify = require('babelify');
+const browserify = require('gulp-browserify');
+
+const config = require('./config/build.config.js');
+const isCi = process.argv.includes('--ci');
  
 gulp.task('scripts:src', () => {
-	return gulp.src(['./src/*.js', './src/layouts/*.js'])
+	return gulp.src(config.path.src)
 		.pipe(babel({
 			presets: ['es2015']
 		}))
@@ -33,8 +38,15 @@ gulp.task('scripts:examples', () => {
 		}));
 });
 
-gulp.task('watch', () => {
-	gulp.watch(['./src/*.js', './src/**/*.js', './examples/**/*.js'], ['scripts:src', 'scripts:examples']);
+gulp.task('lint', () => {
+	gulp.src(config.path.src)
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(gulpIf(isCi, eslint.failAfterError()));
 });
 
-gulp.task('default', ['scripts:src', 'scripts:examples', 'watch']);
+gulp.task('watch', () => {
+	gulp.watch([config.path.src, './examples/**/*.js'], ['scripts:src', 'scripts:examples', 'lint']);
+});
+
+gulp.task('default', ['scripts:src', 'scripts:examples', 'lint', 'watch']);
